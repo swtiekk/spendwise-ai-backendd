fe27ae12631ddd839ca542d5abc4473d7415e6bf
swtiekk <skatrinagolez@gmail.com>
2026-04-18 18:18:44 +0800

    admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import UserProfile, Category, Expense, Income, SavingsGoal, Alert, MLInsight, SmartPurchaseLog
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Category)
+admin.site.register(Expense)
+admin.site.register(Income)
+admin.site.register(SavingsGoal)
+admin.site.register(Alert)
+admin.site.register(MLInsight)
+admin.site.register(SmartPurchaseLog)
